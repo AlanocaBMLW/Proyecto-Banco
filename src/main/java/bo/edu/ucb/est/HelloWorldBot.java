06426082cd914dc62d1800794e68af4992290f0c
BMLW <solaror50@gmail.com>
2021-09-21 23:27:49 -0400

    Final funcionando

--- src/main/java/bo/edu/ucb/est/HelloWorldBot.java
+++ src/main/java/bo/edu/ucb/est/HelloWorldBot.java
@@ -5,7 +5,6 @@
  */
 package bo.edu.ucb.est;
 
-import java.util.List;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -16,6 +15,10 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
  * @author ecampohermoso
  */
 public class HelloWorldBot extends TelegramLongPollingBot {
+	boolean flag = true;
+	boolean flag1 = true;
+	int primer = 0;
+	int segundo = 0;
 
 	@Override
 	public String getBotToken() {
@@ -26,26 +29,37 @@ public class HelloWorldBot extends TelegramLongPollingBot {
 	public void onUpdateReceived(Update update) {
 		SendMessage message = new SendMessage();
 		message.setChatId(update.getMessage().getChatId().toString());
+
 		try {
-			if (update.hasMessage()) { // Verificamos que tenga mensaje
+			if (update.getMessage().getText().equals("Hola") && update.hasMessage()) { // Verificamos que tenga
+																						// mensaje y sea /start
 				message.setText(mensajeInicial());
 
 				execute(message); // Envia el mensaje con el mensaje inicial
 
-			}
-			if (update.getMessage().getText().toString().equals("1")) {
+			} else if (update.getMessage().getText().toString().equals("1") && flag && flag1) {// true entonces entra
+																								// false y
+				// recibe numeros
+
+				flag = false; // sera false cuando entre al 1er mensaje
 				message.setText("Ingrese el 1er numero");
 				execute(message);
-				int primer=0;
-				int segundo=0;
-				primer=Integer.parseInt(update.getMessage().getText().toString());
+			} else if (isNumeric(update.getMessage().getText().toString()) && !flag) {
+
+				primer = Integer.parseInt(update.getMessage().getText().toString());
 				message.setText("Ingrese el 2do numero");
 				execute(message);
-				segundo=Integer.parseInt(update.getMessage().getText().toString());
-				message.setText("La suma es = "+(primer+segundo));
-				execute(message);
+				flag = true;
+				flag1 = false;
+			} else if (isNumeric(update.getMessage().getText().toString()) && !flag1 && flag) {// entra a este 2do flag
+																								// para agarrar el 2do
+																								// numero.
 
-			} else if (update.getMessage().getText().toString().equals("2")) {
+				segundo = Integer.parseInt(update.getMessage().getText().toString());
+				message.setText("La suma es = " + (primer + segundo));
+				execute(message);
+				flag1 = true;
+			} else if (update.getMessage().getText().toString().equals("2") && flag && flag1) {
 				message.setText("funcionalidad no Implementada vuelva otro dia\n");
 				execute(message);
 			}
@@ -54,10 +68,14 @@ public class HelloWorldBot extends TelegramLongPollingBot {
 		}
 	}
 
+	public boolean isNumeric(String num) {
+		return num.matches("-?\\d+(\\.\\d+)?"); // numero entre 0 y 9
+	}
+
 	public String mensajeInicial() {
 		String mensaje;
 		mensaje = "Bienvenido al bot Calculadora\n Seleccione una de las siguientes opciones:"
-				+ "\n1. Sumar dos numeros\n2.Calcular serie fibonacci\n";
+				+ "\n 1. Sumar dos numeros\n2.Calcular serie fibonacci\n";
 
 		return mensaje;
 	}
